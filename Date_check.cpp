b7b5872d905d00bd2a9e5253d907f59eaa86320b
Tudor Stefan <andrei.t.stefan@gmail.com>
2017-10-27 15:48:39 +0300

    Create Date_check.cpp

---
+++ Date_check.cpp
@@ -0,0 +1,36 @@
+/*
+* Sa scrii o functie care ia parametru o data ca si string si sa zica daca este sau nu valida.
+* Ex: f(“21.10.2017”)
+* - implica spart de string dupa punct, transformat in numere si pe urma comparate.
+* in prima faza poti presupune ca toate lunile au 31 zile
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool validDate (string date)
+{
+    bool vDAte;
+    string date = "20.03.2014";
+
+
+
+
+
+    return vDAte;
+
+}
+
+
+int main ()
+{
+
+
+
+
+    return 0;
+}
